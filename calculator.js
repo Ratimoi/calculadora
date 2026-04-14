1c6120c725301c56d52c7014c91ac01094045301
Carlos Eduardo <depauladammerow@gmail.com>
2026-04-14 20:34:43 -0300

    Atualiza a função div

--- calculator.js
+++ calculator.js
@@ -7,7 +7,12 @@ function sub(v1, v2) {
 }
 
 // retorna o valor da divisão entre os dois parâmetros
-function div(v1, v2) {}
+function div(v1, v2) {
+    if (v2 === 0) {
+        return "Erro: divisão por zero";
+    }
+    return v1 / v2;
+}
 
 // retorna o valor da multiplicação entre os dois parâmetros
 function mult(v1, v2) {}
